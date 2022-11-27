433d42890e6741776a4cc8c358c4447ea0f1db51
Elena Navarro <99991881+ElenaaNavarroo@users.noreply.github.com>
2022-11-27 21:24:17 +0000

    Primera version kata 5 parte 2

---
+++ src/kata4/main/Kata4.java
@@ -0,0 +1,40 @@
+package kata4.main;
+
+import java.util.List;
+import kata4.model.Histogram;
+import kata4.model.Mail;
+import kata4.view.HistogramDisplay;
+import kata4.view.MailHistogramBuilder;
+import kata4.view.MailListReader;
+
+public class Kata4 {
+    
+    private static String fileName;
+    private static List<Mail> mailList;
+    private static Histogram<String> histogram;
+    private static HistogramDisplay histoDisplay;
+
+    public static void main(String[] args) {
+        fileName = "email.txt";
+        execute();
+    }
+    
+    private static void execute() {
+        input();
+        process();
+        output();
+        histoDisplay.execute();
+    }
+    
+    private static void input() {
+        mailList = MailListReader.read(fileName);
+    }
+    
+    private static void process() {
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    private static void output() {
+        histoDisplay = new HistogramDisplay(histogram);
+    }
+}
